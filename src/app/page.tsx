c009c5a37a27526683ad81b5fd3e7899be0a0ff6
xTff <mscott2003@gmail.com>
2026-07-29 19:24:15 -0400

    Adding TailwindCSS to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import styles from "./page.module.css";
+import Header from "./components/header/header";
 
 export default function Home() {
   return (
     <div>
-      
+      <Header />
     </div>
   );
 }
