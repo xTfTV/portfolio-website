a65ca9fcf62fdcbe87a32c17f335ab580d9c47a7
xTfTV <mscott2003@icloud.com>
2026-08-07 19:03:18 -0400

    finishing

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,17 @@
 import Header from "../components/header/header";
 
+import { redirect } from 'next/navigation';
+import { getSession } from '@/lib/session';
 
+export default async function Projects() {
 
-export default function Projects() {
+    // Adding the session auth for the page
+    const session = await getSession();
+
+    if (!session) {
+        redirect("/");
+    }
+
     return (
         <div>
             <title>Projects</title>
