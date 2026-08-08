ffa314ab38d7f548b74e9c930e640610ea6f60f9
xTfTV <mscott2003@icloud.com>
2026-08-08 11:02:24 -0400

    ft ✨ - adding the design for the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,9 +14,35 @@ export default async function Dashboard() {
     }
 
     return (
-        <div>
+        <div className="min-h-screen">
             <title>Dashboard</title>
+
             <Header />
-        </div>
+
+            <main className="flex w-full justify-center px-4 py-8 sm:px-6 lg:px-8">
+                <div className="grid w-full max-w-5xl grid-cols-1 gap-4 md:grid-cols-6">
+          
+                <div className="min-h-42 rounded-3xl bg-[#1f1f1f] p-6 md:col-span-2">
+                    Welcome
+                </div>
+
+                <div className="min-h-42 rounded-3xl bg-[#1f1f1f] p-6 md:col-span-4">
+                    Projects Due
+                </div>
+
+                <div className="min-h-52 rounded-3xl bg-[#1f1f1f] p-6 md:col-span-2">
+                    Critical
+                </div>
+
+                <div className="min-h-64 rounded-3xl bg-[#1f1f1f] p-6 md:col-span-4 md:row-span-2">
+                    Project Creation
+                </div>
+
+                <div className="min-h-52 rounded-3xl bg-[#1f1f1f] p-6 md:col-span-2">
+                    Due Soon
+                </div>
+            </div>
+        </main>
+    </div>
     );
 }
